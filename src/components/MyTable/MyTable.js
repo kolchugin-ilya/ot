2eeff65339da514ef0bdafa1617ba66db5a170fd
ilya <kolchugin.ilya@gmail.com>
2021-11-26 10:34:02 +0300

    Страница список сотрудников, добавление кнопок

--- src/components/MyTable/MyTable.js
+++ src/components/MyTable/MyTable.js
@@ -1,52 +1,15 @@
 import React, {useEffect} from 'react';
 import MaterialTable from "material-table";
-import {Close, Edit, Menu, Trash} from "grommet-icons";
-import styles from './MyTable.module.css';
-import {Button} from "grommet";
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import {setCloseButton} from "../../store/actions/modal-actions";
-import Modal from "../../layout/Modal/Modal";
 
-const MyTable = ({data}) => {
+const MyTable = ({data, header, title}) => {
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(setCloseButton('close'))
     }, [dispatch])
 
-    // const data = [];
-    // for (let i = 1; i < 30; i++) {
-    //     data.push({
-    //         icons: <div className={styles.icons}>
-    //             <Button icon={<Edit size="35x" color="#74cf70"/>}
-    //                     onClick={() =>
-    //                     {
-    //                         dispatch(setCloseButton('show'))
-    //                     }
-    //                     }/>
-    //             <Button icon={<Trash size="35x" color="#f76f57"/>}
-    //                     onClick={() =>
-    //                         alert("delete")}/>
-    //         </div>,
-    //         name: "ФИО" + i,
-    //         bdate: "0" + i + ".0" + i + ".1998",
-    //         position: "pos" + i,
-    //         podr: "podr" + i
-    //     })
-    // }
-    const header = [
-        {title: '', field: 'ID'},
-        {title: 'ФИО', field: 'LAST_NAME'},
-        {title: 'Дата рождения', field: 'bdate', type: 'FIRST_NAME'},
-        {title: 'Должность', field: 'position'},
-        {title: 'Подразделение', field: 'podr'}
-    ]
-    // const payload = [{
-    //     name: "ФИО",
-    //     bday: "23123213",
-    //     position: "1123123",
-    //     podr: "podr"
-    // }]
     return (
         <div style={{width: '100%'}}>
             <MaterialTable
@@ -71,7 +34,7 @@ const MyTable = ({data}) => {
                 }}
                 columns={header}
                 data={data}
-                title="Список сотрудников"
+                title={title}
             />
         </div>
     );
--- src/layout/Pages/Employers/Employers.js
+++ src/layout/Pages/Employers/Employers.js
@@ -1,33 +1,64 @@
 import React, {useEffect} from 'react';
 import MyTable from "../../../components/MyTable/MyTable";
-import {dataEmployers, headersEmployers} from "../../../data";
 import Modal from "../../Modal/Modal";
 import axios from "axios";
 import {useDispatch, useSelector} from "react-redux";
 import {setEmployers} from "../../../store/actions/data-actions";
+import {Edit, Trash} from "grommet-icons";
+import {Button} from "grommet";
 
 const Employers = () => {
-    const { employers } = useSelector(state => state.dataReducer)
+    const {employers} = useSelector(state => state.dataReducer)
     const dispatch = useDispatch()
 
-    useEffect(() => {
+    const readData = () => {
         axios.post("http://localhost:3001/read", {
-            table: "EMPLOYERS"
+            table: "EMPLOYERS",
+            columns: "ID, LAST_NAME, FIRST_NAME, OTC, BIRTHDAY, POSITION, PODR_ID"
         }, {withCredentials: true})
             .then(response => {
-                    console.log(response.data.result)
-                    dispatch(setEmployers(response.data.result))
+                let employers = [];
+                response.data.result.map(emp => {
+                    employers.push(Object.assign({
+                        icons:
+                            <div style={{display: "flex"}}>
+                                <Button icon={<Edit size="35x" color="#74cf70"/>}
+                                        onClick={() =>
+                                            alert("edit" + emp.ID)}/>
+                                <Button icon={<Trash size="35x" color="#f76f57"/>}
+                                        onClick={() =>
+                                            alert("delete")}/>
+                            </div>
+                    }, emp))
+                })
+                dispatch(setEmployers(employers))
             })
             .catch(error => {
                 console.log("check login error", error);
             });
-        console.log(employers)
-    },[])
+    }
+
+    const header = [
+        {title: '', field: 'icons'},
+        {title: 'Фамилия', field: 'LAST_NAME'},
+        {title: 'Имя', field: 'LAST_NAME'},
+        {title: 'Отчество', field: 'OTC'},
+        {title: 'Дата рождения', field: 'BIRTHDAY', type: 'date'},
+        {title: 'Должность', field: 'POSITION'},
+        {title: 'Подразделение', field: 'PODR_ID'}
+    ]
+
+    useEffect(() => {
+        readData();
+    }, [])
 
     return (
         <>
             <Modal/>
-            <MyTable data={employers}
+            <MyTable
+                title='Список сотрудников'
+                data={employers}
+                header={header}
             />
         </>
     );
